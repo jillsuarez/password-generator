2b06f6082bbcfca720538a6df69815caa20489c8
jillsuarez <jillsuarez@live.com>
2021-10-21 17:41:57 -0400

    added userChoices

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,92 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+// array of upperChar
+var upperCasedCharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+];
+// array of lowerChar
+var lowerCasedCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
+// array of numericChar
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+// array of specialChar
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.'
+];
 
 
 function generatePassword () {
@@ -30,23 +116,42 @@ function generatePassword () {
 function getUserOptions () {
   var userChoices = {};
   // ask the user, using prompts, password criteria
-  prompt('How long would you like your password to be (between 8-128 for best security)?')
-
+  
   // ask length
   // calidate between 8-128
+  var length = prompt('How long would you like your password to be (between 8-128 for best security)?')
+  
+  // to do: validate length
+
+  //assign to userOptions
+  userChoices.length = parseInt(length);
 
   // chose if to include lowercase
+  var lowerCase = confirm('Do you want lower case characters in your password?');
+  userChoices.lowerCase = lowerCase;
+
   // chose if to include uppercase
+  var upperCase = confirm('Do you want upper case characters in your password?');
+  userChoices.upperCase = upperCase;
+
   // chose if to include numeric
+  var numeric = confirm('Do you want numeric characters in your password?');
+  userChoices.numeric = numeric;
+
   // chose if to include and/or special characters
+  var special = confirm('Do you want special characters in your password?');
+  userChoices.special = special;
   
   // validate user choses at least one character type
+
+  return userChoices;
 }
 
 
 // Write password to the #password input
 function writePassword() {
-  
+  var userChoices = getUserOptions();
+  console.log(userChoices);
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
