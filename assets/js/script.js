56c27e5dd6fa6ccbebfda0b62bf1680a9793c3d5
jillsuarez <jillsuarez@live.com>
2021-10-21 13:58:24 -0400

    first draft pseudo js

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,5 +13,41 @@ function writePassword() {
 
 }
 
+function generatePassword () {
+  var userOptions = getUserOptions ();
+
+  // check if upper
+    // add a random char from upper
+
+  // check if lower
+    // add a random char from lower
+
+  // check if alpha
+    // add a random char from alpha
+
+  // check if special
+    // add a random char from special
+
+  //for loop the rest randomly to list
+
+  // generate password
+
+  // return password
+}
+
+function getUserOptions () {
+  // ask the user, using prompts, password criteria
+
+  // ask length
+  // calidate between 8-128
+
+  // chose if to include lowercase
+  // chose if to include uppercase
+  // chose if to include numeric
+  // chose if to include and/or special characters
+  
+  // validate user choses at least one character type
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
